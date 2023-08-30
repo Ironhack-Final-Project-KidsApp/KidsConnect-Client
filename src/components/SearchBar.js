c724a82678936b24a815159a2e920844ff999616
Inês Almeida <ines_almeida_203@hotmail.com>
2023-08-30 23:37:50 +0100

    searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,29 @@
-/* function SearchBar({ searchQuery, setSearchQuery }) {
-    return (
-      <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Search for activities"/>
-    );
-  }
-  
-  export default SearchBar; */
+import { FaSearch } from 'react-icons/fa'
+import './SearchBar.css';
+import { useState } from 'react';
+import AllActivitiesList from '../pages/AllActivitiesList';
+
+function SearchBar({activitiesList}) {
+    const [searchItem, setSearchItem] = useState("");
+    const [filteredActivities, setFilteredActivities] = useState([]);
+
+
+    const handleSearchInput = (e) => {
+        const searchText = e.target.value.toLowerCase();
+        const filtered  = searchItem.filter((activity ) =>
+        activity.name.toLowerCase().includes(searchText)
+        );
+        setFilteredActivities(filtered);
+        setSearchItem(searchText);
+      }; 
+
+    return(
+        <div className="input-wrapper">
+            <FaSearch id='search-icon'/>
+            <input placeholder='Search for activities...' value={searchItem} onChange={handleSearchInput}/>
+            <AllActivitiesList activitiesList={searchItem ? filteredActivities : activitiesList} />
+        </div>
+    )
+}
+
+export default SearchBar;
