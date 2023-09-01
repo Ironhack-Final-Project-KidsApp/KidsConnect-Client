b1e89a969b01a8c30c6193647bb800af9d48ebab
Inês Almeida <ines_almeida_203@hotmail.com>
2023-09-01 10:34:47 +0100

    activity ActivityCard

--- src/components/ActivityCard.js
+++ src/components/ActivityCard.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
-<<<<<<< HEAD
-function ActivityCard({ activity }) {
+
+/* ines code - function ActivityCard({ activity }) {
   return (
     <div key={activity._id}>
       <img src={activity.image} alt="activity-img" />
@@ -12,14 +12,14 @@ function ActivityCard({ activity }) {
       <p>Created by: {activity.author}</p>
     </div>
   );
-}
+} */
 
-=======
+
 const ActivityCard = (props) => {
     return (
         <div><Link to={`/activity/${props.item._id}`}>{props.item.title}</Link></div>
     );
 }
  
->>>>>>> d7282c77d8d7ea2d8d49fc5a1bdee776e5b0dd10
+
 export default ActivityCard;
