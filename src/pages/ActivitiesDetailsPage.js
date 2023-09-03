8860a0a73bd3d6491b465d6f53e92e47bbdfd4c4
Inês Almeida <ines_almeida_203@hotmail.com>
2023-09-03 23:42:56 +0100

    favorites/logout redirect

--- src/pages/ActivitiesDetailsPage.js
+++ src/pages/ActivitiesDetailsPage.js
@@ -7,44 +7,50 @@ import { AuthContext } from '../context/auth.context';
 const ActivitiesDetailsPage = () => {
     const authContext = useContext(AuthContext);
     const { user } = authContext;
-
-    const params = useParams();
-    const { idactivity } = params;
+    const { idActivity } = useParams();
     const [activity, setActivity] = useState(null);
     const [isFavorite, setIsFavorite] = useState(false);
 
-    // Function to fetch activity details by ID
-    const fetchActivityDetails = async () => {
-        try {
-            const response = await activityService.getActivity(idactivity);
-            setActivity(response.data);
-        } catch (error) {
-            console.error("Error fetching activity details:", error);
-        }
-    };
-
-    // Function to toggle favorite status
-    const toggleFavorite = async () => {
-        try {
-            // Call the toggleFavorite API endpoint
-            await userService.toggleFavorite(idactivity);
-
-            // Update the isFavorite state
-            setIsFavorite(!isFavorite);
-        } catch (error) {
-            console.error("Error toggling favorite:", error);
-        }
-    };
-
+    console.log('user is:', user)
+    console.log('activity is:', idActivity)
+    
+    //fetch activity details - done
     useEffect(() => {
-        fetchActivityDetails();
-        // Check if the activity is in the user's favorites and update isFavorite state accordingly
-        if (user && user.favorite.includes(idactivity)) {
-            setIsFavorite(true);
-        }
-    }, [idactivity, user]);
+      activityService.getActivity(idActivity)
+      .then((response) => {
+        console.log(response.data)
+        setActivity(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }, [idActivity])
 
-    return (
+    if (!activity) {
+      return <p>Loading...</p>;
+    }
+
+    //handle the favorite button
+    const handleFavoriteButton = () => {
+      try {
+        if (isFavorite) {
+          userService.removeFavoriteActivity(idActivity)
+            .then(() => setIsFavorite(false))
+            .catch((err) => console.error(err));
+        } else {
+          userService.addFavoriteActivity(idActivity)
+            .then(() => setIsFavorite(true))
+            .catch((err) => console.error(err));
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+     
+
+//display activity details
+//display button to add and remove the activity from the favourites depending on the user id    
+return (
         <div>
             {activity && (
                 <>
@@ -55,7 +61,7 @@ const ActivitiesDetailsPage = () => {
                     <p>Min. Age: {activity?.ageMin}</p>
                     <p>Max. Age: {activity?.ageMax}</p>
                     <p>Location: {activity?.location}</p>
-                    <button onClick={toggleFavorite}>
+                    <button onClick={handleFavoriteButton}>
                         {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
                     </button>
                 </>
