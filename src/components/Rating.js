b284dbdb3820a78b86e8900bbeba77b58393f1f7
Inês Almeida <ines_almeida_203@hotmail.com>
2023-09-05 23:09:39 +0100

    rating done

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,12 +3,11 @@ import { FaStar } from 'react-icons/fa';
 import './Rating.css';
 import rateService from "../services/rate.services";
 
-const Rating = ({ idactivity }) => {
+const Rating = ({ idactivity, onUpdateAverageRating }) => {
     const [rating, setRating] = useState(null);
     const [hover, setHover] = useState()
 
     useEffect(() => {
-        // Fetch the user's current rating for the activity when the component mounts
         const fetchUserRating = async () => {
           try {
             const response = await rateService.userRateForActivity(idactivity);
@@ -26,7 +25,7 @@ const Rating = ({ idactivity }) => {
         try {
             await rateService.createRate(idactivity, { rate: item });
             setRating(item);
-
+            onUpdateAverageRating();
         } catch (error) {
             console.error("Error rating the activity:", error);
         }
--- src/pages/ActivitiesDetailsPage.js
+++ src/pages/ActivitiesDetailsPage.js
@@ -1,17 +1,28 @@
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
 import activityService from '../services/activity.services';
 import FavoriteButton from "../components/FavoriteButton";
 import Rating from "../components/Rating";
 import rateService from "../services/rate.services";
 
 const ActivitiesDetailsPage = () => {
-    const { idactivity } = useParams();
-    const [activity, setActivity] = useState(null);   
-    const [averageRating, setAverageRating] = useState(null); 
+  const { idactivity } = useParams();
+  const [activity, setActivity] = useState(null);
+  const [averageRating, setAverageRating] = useState(null);
 
-    useEffect(() => {
-      activityService.getActivity(idactivity)
+  const fetchAverageRating = () => {
+    rateService.avarageRate(idactivity)
+      .then((response) => {
+        console.log('rate response', response.data.result)
+        setAverageRating(response.data.result);
+      })
+      .catch((error) => {
+        console.error("Error fetching average rating:", error);
+      });
+  };
+
+  useEffect(() => {
+    activityService.getActivity(idactivity)
       .then((response) => {
         setActivity(response.data)
       })
@@ -19,42 +30,39 @@ const ActivitiesDetailsPage = () => {
         console.log(error);
       });
 
-      rateService.avarageRate(idactivity)
-      .then((response) => {
-        console.log('rate response', response.data.result)
-          setAverageRating(response.data.result);
-      })
-      .catch((error) => {
-          console.error("Error fetching average rating:", error);
-      });
-    }, [idactivity])
+    fetchAverageRating();
+  }, [idactivity]);
 
-    if (!activity) {
-      return <p>Loading...</p>;
-    }
-    
-return (
-        <div>
-            {activity && (
-                <>
-                    <img src={activity?.image} alt="activity-img" />
-                    <h1>{activity?.title}</h1>
-                    <p>Description: {activity?.description}</p>
-                    <p>Stroller: {activity?.stroller ? "Yes" : "No"}</p>
-                    <p>Min. Age: {activity?.ageMin}</p>
-                    <p>Max. Age: {activity?.ageMax}</p>
-                    <p>Location: {activity?.location}</p>
-                    <p>Priced: {activity?.priced ? "Yes" : "No"}</p>
-                    <p>Venue type: {activity?.venuetype}</p>
-                    <FavoriteButton idactivity={idactivity}/>
-                    <Rating idactivity={idactivity}/>
-                    {averageRating !== null && (
-                        <p>Average Rating: {averageRating}</p>
-                    )}
-                </>
-            )}
-        </div>
-    );
+  const updateAverageRating = () => {
+    fetchAverageRating();
+  };
+
+  if (!activity) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      {activity && (
+        <>
+          <img src={activity?.image} alt="activity-img" />
+          <h1>{activity?.title}</h1>
+          <p>Description: {activity?.description}</p>
+          <p>Stroller: {activity?.stroller ? "Yes" : "No"}</p>
+          <p>Min. Age: {activity?.ageMin}</p>
+          <p>Max. Age: {activity?.ageMax}</p>
+          <p>Location: {activity?.location}</p>
+          <p>Priced: {activity?.priced ? "Yes" : "No"}</p>
+          <p>Venue type: {activity?.venuetype}</p>
+          <FavoriteButton idactivity={idactivity} />
+          <Rating idactivity={idactivity} onUpdateAverageRating={updateAverageRating} />
+          {averageRating !== null && (
+            <p>Average Rating: {averageRating}</p>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default ActivitiesDetailsPage;
